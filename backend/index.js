e476c0edc214ae55d46ef8fca00c114db3489d22
Vibhor Singh <vibhor.formal@gmail.com>
2022-03-24 21:23:09 +0530

    Backend updated

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+ const express = require('express')
+ const mongoose= require('mongoose')
+
+ const app=express()
+
+ const DB='mongodb+srv://VibhorSingh:Vibh0r$ingh@cluster0.shi3y.mongodb.net/fitness?retryWrites=true&w=majority'
+ mongoose.connect(DB,{
+     useNewUrlParser: true,
+    //  useCreateIndex: true,
+     useUnifiedTopology: true,
+    //  useFindAndModify: false
+ }).then(()=>{
+     console.log("DB connection successfull");
+ }).catch((err)=>console.log(err));
+ 
+ const Userroute=require('./routers/Userroute');
+
+ app.use('/user',Userroute)
+ app.listen(4000,() => {
+    console.log("Started on PORT 4000");
+    })
